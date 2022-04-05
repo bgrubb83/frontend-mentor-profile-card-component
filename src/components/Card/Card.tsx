2255b87a3cd13b5d95a34663899a109a8b4851f1
Barry Grubb <bgrubb83@gmail.com>
2022-04-05 21:44:14 +0100

    Remove unneccessary CRA stuff, create empty Card component.

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Card from './components/Card'
+import './Card.css';
 
 function App() {
-  return (
-    <Card />
-  );
+    return (
+        <div>Card</div>
+    );
 }
 
 export default App;
